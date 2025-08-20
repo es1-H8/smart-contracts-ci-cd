215df57beddbc6299e3c4d38052fa4244eb5131a
Shazil-Web3 <shazilsaddique72.com>
2025-08-21 00:15:36 +0500

    first com

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -16,16 +16,17 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 5,
-      gasPrice: 20000000000, // 20 gwei
-    },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
+      gasPrice: 15000000000, // 15 gwei - optimized for Sepolia
+      gas: 3000000, // 3M gas limit
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 5,
       gasPrice: 20000000000, // 20 gwei
     },
     mainnet: {
@@ -37,8 +38,8 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.ETHERSCAN_API_KEY || "",
       sepolia: process.env.ETHERSCAN_API_KEY || "",
+      goerli: process.env.ETHERSCAN_API_KEY || "",
       mainnet: process.env.ETHERSCAN_API_KEY || "",
     },
   },
